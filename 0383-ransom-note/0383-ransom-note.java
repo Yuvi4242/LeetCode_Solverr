cee533ab3ce5278ff435395ee1951d1b44d3979f
Yuvraj Singh <156509546+Yuvi4242@users.noreply.github.com>
2026-08-01 13:27:17 +0530

    Time: 14 ms (34.56%), Space: 46.8 MB (38.93%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,11 +1,20 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int[]freq=new int[26];
+        // int[]freq=new int[26];
+        // for(char ch:magazine.toCharArray()){
+        //     freq[ch-'a']++;
+        // }
+        // for(char ch:ransomNote.toCharArray()){
+        //     if(--freq[ch-'a']<0) return false;
+        // }
+        // return true;
+        HashMap<Character,Integer>map=new HashMap<>();
         for(char ch:magazine.toCharArray()){
-            freq[ch-'a']++;
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
         for(char ch:ransomNote.toCharArray()){
-            if(--freq[ch-'a']<0) return false;
+            if(!map.containsKey(ch)||map.get(ch)==0) return false;
+            map.put(ch,map.get(ch)-1);
         }
         return true;
     }
