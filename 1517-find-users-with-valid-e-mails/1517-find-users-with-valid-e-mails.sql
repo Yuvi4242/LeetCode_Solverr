6178fdb06e78bd597cde21e5f9823b1cb93f1c4e
Yuvraj Singh <156509546+Yuvi4242@users.noreply.github.com>
2026-08-18 20:12:23 +0530

    Time: 868 ms (41%), Space: 0B (100%) - LeetHub

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -1,7 +1,11 @@
-SELECT *
-FROM Users
-WHERE REGEXP_LIKE(
-    mail,
-    '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$',
-    'c'
-);
+select * from Users
+where mail regexp '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$' 
+and mail like binary '%@leetcode.com';
+
+-- SELECT *
+-- FROM Users
+-- WHERE REGEXP_LIKE(
+--     mail,
+--     '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$',
+--     'c'
+-- );
