3d87638a8069939a2a688d0e08340ce841794ef4
Yuvraj Singh <156509546+Yuvi4242@users.noreply.github.com>
2026-09-07 21:05:10 +0530

    Time: 118 ms (49.52%), Space: 214.7 MB (68%) - LeetHub

--- 3843-first-element-with-unique-frequency/3843-first-element-with-unique-frequency.java
+++ 3843-first-element-with-unique-frequency/3843-first-element-with-unique-frequency.java
@@ -1,16 +1,28 @@
 class Solution {
     public int firstUniqueFreq(int[] nums) {
+        // HashMap<Integer,Integer>map=new HashMap<>();
+        // for(int num:nums){
+        //     map.put(num,map.getOrDefault(num,0)+1);
+        // }
+        // HashMap<Integer,Integer>freq=new HashMap<>();
+        // for(int fre:map.values()){
+        //     freq.put(fre,freq.getOrDefault(fre,0)+1);
+        // }
+        // for(int num:nums){
+        //     int f=map.get(num);
+        //     if(freq.get(f)==1)return num;
+        // }
+        // return -1;
         HashMap<Integer,Integer>map=new HashMap<>();
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
         HashMap<Integer,Integer>freq=new HashMap<>();
-        for(int fre:map.values()){
-            freq.put(fre,freq.getOrDefault(fre,0)+1);
+        for(int n:nums){
+            map.put(n,map.getOrDefault(n,0)+1);
         }
-        for(int num:nums){
-            int f=map.get(num);
-            if(freq.get(f)==1)return num;
+        for(int m:map.values()){
+            freq.put(m,freq.getOrDefault(m,0)+1);
+        }
+        for(int i=0;i<nums.length;i++){
+            if(freq.get(map.get(nums[i]))==1)return nums[i];
         }
         return -1;
     }
