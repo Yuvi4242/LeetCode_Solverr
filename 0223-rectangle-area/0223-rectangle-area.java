621ad559791a2c1c44f72bc764cbc8205d9285d9
Yuvraj Singh <156509546+Yuvi4242@users.noreply.github.com>
2026-09-14 20:23:36 +0530

    Time: 2 ms (68.64%), Space: 44.3 MB (67.72%) - LeetHub

---
+++ 0223-rectangle-area/0223-rectangle-area.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int a1=(ax2-ax1)*(ay2-ay1);
+        int a2=(bx2-bx1)*(by2-by1);
+        int overWidth=Math.max(0,Math.min(ax2,bx2)-Math.max(ax1,bx1));
+        int overHeight=Math.max(0,Math.min(ay2,by2)-Math.max(ay1,by1));
+        int overlap=overWidth*overHeight;
+        return a1+a2-overlap;
+    }
+}
